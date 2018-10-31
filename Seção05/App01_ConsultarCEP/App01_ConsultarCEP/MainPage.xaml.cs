811f63ee0c4f6353acfc34e349fbc05a2571ea7e
anderson.lari.davi <anderson.lari.davi@hotmail.com>
2018-10-31 00:21:59 -0300

    Adicionado validações

--- Seção05/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
+++ Seção05/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -26,8 +26,33 @@ namespace App01_ConsultarCEP
 
             //TODO - Consulta a API
             string cep = CEP.Text.Trim();
-            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(CEP.Text);
-            RESULTADO.Text = string.Format("Endereço : {0}, {1} {2}", end.localidade, end.uf, end.logradouro);
+            if (isValidCEP(cep))
+            {
+                Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(CEP.Text);
+                RESULTADO.Text = string.Format("{2} - Bairro {3} - {0}/{1}", end.localidade, end.uf, end.logradouro, end.bairro);
+            }
+        }
+
+        private bool isValidCEP(string CEP)
+        {
+            bool valido = true;
+
+            //TODO - Validar se tem 8 posições
+            if (CEP.Length != 8)
+            {
+                //TDOD - Tratar erro
+                DisplayAlert("ERRO", "CEP Inválido! O CEP Deve conter 8 caracteres.","Ok");
+                valido = false;
+            }
+            //TODO - Validar se é numérico 
+            int NovoCEP = 0;
+            if (!int.TryParse(CEP, out NovoCEP))
+            {
+                //TDOD - Tratar erro
+                DisplayAlert("ERRO", "CEP Inválido! O CEP Deve deve ser composto apenas por números.", "Ok");
+                valido = false;
+            }
+            return valido;
         }
     }
 }
